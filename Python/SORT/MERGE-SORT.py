4472d7b5a0efebdcb5840b8205781e459517cdaa
cp-yu <68734436+cp-yu@users.noreply.github.com>
2022-07-03 21:05:43 +0800

    add rewrite MERGE

--- Python/SORT/MERGE-SORT.py
+++ Python/SORT/MERGE-SORT.py
@@ -28,4 +28,64 @@ def MERGE_SORT(A,p,r):
         A=MERGE(A,p,q,r)
     return A
 
+def MERGE_2(A, p, q, r):
+    n1 = q - p + 1
+    n2 = r - q
+    L = [0] * (n1)
+    R = [0] * (n2)
+
+    for i in range(n1):
+        L[i] = A[p + i]
+
+    for i in range(n2):
+        R[i] = A[q + i + 1]
+    i = 0
+    j = 0
+    for k in range(p, r + 1):
+        # I think "try" will play well here
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i = i + 1
+            if i >= n1:
+                A[k + 1:r+1] = R[j:]
+                break
+        else:
+            A[k] = R[j]
+            j = j + 1
+            if j >= n2:
+                A[k + 1:r+1] = L[i:]
+                break
+
+    return A
+
+def MERGE_3(A, p, q, r):
+    n1 = q - p + 1
+    n2 = r - q
+    L = [0] * (n1)
+    R = [0] * (n2)
+    
+    for i in range(n1):
+        L[i] = A[p + i]
+    for i in range(n2):
+        R[i] = A[q + i + 1]
+
+    i = 0
+    j = 0
+    for k in range(p, r + 1):
+        try:
+            if L[i] <= R[j]:
+                A[k] = L[i]
+                i = i + 1
+            else:
+                A[k] = R[j]
+                j = j + 1
+        except IndexError:
+            if i >= n1:
+                A[k :r + 1] = R[j:]
+                break
+            else:
+                A[k :r + 1] = L[i:]
+                break
+    return A
+
 # A=MERGE_SORT(A,0,len(A)-1)
